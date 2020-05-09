d59b53021a8f86758876ad5861ee578f9a484690
Lalu Arman Rozika <armanrozika@gmail.com>
2020-05-09 22:08:18 +0700

    V0.0

---
+++ precache-manifest.4bb3ec3d438464ec3a0617942692a47d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b254e7bcf9da08271c143c1c94cb60e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86fbda6713e022027ca5",
+    "url": "/static/css/main.0ff2509a.chunk.css"
+  },
+  {
+    "revision": "0a7d097f8bcf9c339b1d",
+    "url": "/static/js/2.dab42258.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.dab42258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86fbda6713e022027ca5",
+    "url": "/static/js/main.3cd9e8b3.chunk.js"
+  },
+  {
+    "revision": "4a4ca2d16e057cb7324d",
+    "url": "/static/js/runtime-main.3ff1509c.js"
+  }
+]);
