5367691d88ee7042deaf85bc410dd2b83cba0ce9
Lalu Arman Rozika <armanrozika@gmail.com>
2020-05-09 22:48:01 +0700

    Add files via upload

---
+++ precache-manifest.923aa31d7ead47d33267014ed2c4d614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a92ccc47e12fff5a4819ff4d2f684c",
+    "url": "/tex-build/index.html"
+  },
+  {
+    "revision": "eca01df91ac67fa15256",
+    "url": "/tex-build/static/css/main.0ff2509a.chunk.css"
+  },
+  {
+    "revision": "0a7d097f8bcf9c339b1d",
+    "url": "/tex-build/static/js/2.dab42258.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/tex-build/static/js/2.dab42258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca01df91ac67fa15256",
+    "url": "/tex-build/static/js/main.c3ddb2e4.chunk.js"
+  },
+  {
+    "revision": "2f26d2480bc8e5dbbc54",
+    "url": "/tex-build/static/js/runtime-main.eb272225.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4bb3ec3d438464ec3a0617942692a47d.js"
+  "/tex-build/precache-manifest.923aa31d7ead47d33267014ed2c4d614.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tex-build/#/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tex-build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
